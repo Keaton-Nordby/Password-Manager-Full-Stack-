6f1f45ebe5a10eb0fdab0ac62727dd20515983a3
Keaton Nordby <keatonnordby4@gmail.com>
2025-12-16 15:06:22 -0600

    simple ui form added

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className='AddingPassword'>
+        <input type='text' placeholder='Ex. password123'/>
+        <input type='text' placeholder='Ex. Facebook'/>
+        <button>Add Password</button>
+
+      </div>
     </div>
   );
 }
